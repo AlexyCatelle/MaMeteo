afa0e65cffbd9dd156e6b15f20ac6f0a85a047e3
AlexyCFixe <alexy.catelle@oclock.school>
2024-06-24 17:16:05 +0200

    search in progress

--- components/Search.js
+++ components/Search.js
@@ -6,20 +6,20 @@ export default class Search extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            city: 'Lille'
+            city: 'Useless Text',
         };
     };
-
-    setCity(city) {
-        this.setState({city})
+    setCity = (city) => {
+        this.setState({ city });
     };
+
     render() {
         return (
             <SafeAreaView>
                 <TextInput
                     style={styles.input}
                     value={this.state.city}
-                    onChangeText={(text) => this.setCity(text)}
+                    onChangeText={this.state.city}
                 />
             </SafeAreaView>
         );
